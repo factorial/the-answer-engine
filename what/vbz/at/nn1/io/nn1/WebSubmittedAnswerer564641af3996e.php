713ba4c74dcad87e3de22bc12743ccf408756d50
theanswerengine <theanswerengine@gmail.com>
2015-11-13 20:05:22 +0000

    Web user IP 65.182.48.83

---
+++ what/vbz/at/nn1/io/nn1/WebSubmittedAnswerer564641af3996e.php
@@ -0,0 +1,24 @@
+<?php 
+/* 
+what / vbz / at / nn1 / io / nn1 / 
+what / is / article (e.g. the, no) / singular common noun (e.g. book, girl) / of (as preposition) / singular common noun (e.g. book, girl) / 
+*/
+
+class WebSubmittedAnswerer564641af3996e implements Answerer {
+    public static function check_question(array $question) {
+        return false; // This algorithm doesn't do anything yet.
+    }
+
+    public static function answer_question(array $question) {
+        /*
+
+          Author: Web user IP 65.182.48.83
+          Author's original question: 'what is the capital of america'
+
+          Comments:
+
+use the wikipedia api, only answer questions where the first singular common noun is "capital"
+
+        */
+    }
+}
