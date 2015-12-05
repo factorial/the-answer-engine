0b8db930bbfdf36f1dcbd5a3c1a0a25fd4cdefaf
theanswerengine <theanswerengine@gmail.com>
2015-12-05 07:45:56 +0000

    Web user IP 65.182.48.83

---
+++ what/vbz/mc/ii/mc/WebSubmittedAnswerer5662946d5ae58.php
@@ -0,0 +1,24 @@
+<?php 
+/* 
+what / vbz / mc / ii / mc / 
+what / is / cardinal number,neutral for number (two, three..) / general preposition / cardinal number,neutral for number (two, three..) / 
+*/
+
+class WebSubmittedAnswerer5662946d5ae58 implements Answerer {
+    public static function check_question(array $question) {
+        return false; // This algorithm doesn't do anything yet.
+    }
+
+    public static function answer_question(array $question) {
+        /*
+
+          Author: Web user IP 65.182.48.83
+          Author's original question: 'what is two plus two?'
+
+          Comments:
+
+if the preposition is an operator you can script, just write it. let the computer compute!
+
+        */
+    }
+}
